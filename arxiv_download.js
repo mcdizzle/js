884aada628dbf5db3bf89ffe04d216f6a3e0982c
Mckenzie West <mckenzierwest@gmail.com>
2016-08-11 18:39:08 -0700

    arxiv download 0.001

--- arxiv_download.js
+++ arxiv_download.js
@@ -1,7 +1,2 @@
-javascript: alert(getElementsByTagName('meta')[0].content);
+alert(getElementsByTagName('meta')[0].content);
 
-javascript: (function () { 
-    var jsCode = document.createElement('script'); 
-    jsCode.setAttribute('src', 'http://github.com/mcdizzle/js/arxiv_download.js');                  
-  document.body.appendChild(jsCode); 
- }());
